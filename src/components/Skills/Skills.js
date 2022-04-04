3e5eea91f5c05e20ca36761edd34df81abf35d36
DeeJayAdi <deejayadipl@gmail.com>
2022-04-04 10:30:45 +0200

    update

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,8 +1,22 @@
 import "./Skills.css";
 
+const Skill = (props) => (
+  <div className="skill">
+    <div className="skill-circle">
+      <p>{props.percent}%</p>
+    </div>
+    <p>{props.name}</p>
+  </div>
+);
+
 const Skills = (props) => (
   <section className="skills" id="skills">
     <h1>Skills</h1>
+    <div className="skill-list">
+      <Skill name="html" percent={75} />
+      <Skill name="javascript" percent={60} />
+      <Skill name="css" percent={70} />
+    </div>
   </section>
 );
 
