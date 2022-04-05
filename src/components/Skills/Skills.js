845607638066001aa5f400a5749441359443be22
DeeJayAdi <deejayadipl@gmail.com>
2022-04-05 21:10:42 +0200

    update

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,12 +4,13 @@ import "./Skills.css";
 const skills = [
   { name: "html", percent: 80 },
   { name: "javascript", percent: 70 },
-  { name: "nodejs", percent: 50 },
-  { name: "css", percent: 60 },
-  { name: "react js", percent: 65 },
+  { name: "nodejs", percent: 30 },
+  { name: "css", percent: 65 },
+  { name: "react js", percent: 50 },
   { name: "kubernetes", percent: 30 },
   { name: "docker", percent: 40 },
   { name: "git", percent: 50 },
+  { name: "php", percent: 20 },
 ];
 
 const Skills = (props) => (
