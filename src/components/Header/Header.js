11d565d32e7bc0c6e6cedd5922058bace411a5d6
DeeJayAdi <deejayadipl@gmail.com>
2022-04-09 23:26:55 +0200

    update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,7 @@
 import "./Header.css";
 
 const Header = (props) => (
-  <section className="header noselect">
+  <section className="header">
     <h1>Hi, I'm</h1>
     <svg viewBox="0 0 1000 200">
       <text x="50%" y="50%" dy=".15em" textAnchor="middle">
